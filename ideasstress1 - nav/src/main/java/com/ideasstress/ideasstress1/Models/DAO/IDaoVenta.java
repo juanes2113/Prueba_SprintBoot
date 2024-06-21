80fcc1683184b8713a476ad7fbb56d833a31a043
juanes2113 <136110782+juanes2113@users.noreply.github.com>
2024-06-21 16:08:53 -0500

    Delete ideasstress1 - nav directory

--- ideasstress1 - nav/src/main/java/com/ideasstress/ideasstress1/Models/DAO/IDaoVenta.java
+++
@@ -1,8 +0,0 @@
-package com.ideasstress.ideasstress1.Models.DAO;
-
-import com.ideasstress.ideasstress1.Models.Entity.Venta;
-
-public interface IDaoVenta
-{
-    public Venta searchSale(Long Id_Venta);
-}
